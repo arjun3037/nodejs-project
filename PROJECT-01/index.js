ec38bfb65a308c0e3e9484b46702a4d2443ee8fa
Arjun Singh <arjsingh4@publicisgroupe.net>
2023-09-12 14:43:11 +0530

    commit

--- PROJECT-01/index.js
+++ PROJECT-01/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const users = require('./MOCK_DATA.json');
+const fs = require('fs');
 
 const app =  express();
-
 const port = 8000;
+// middleware
+app.use(express.urlencoded({extended:false}))
+
+
+app.use((req,res,next) => {
+    console.log('HEllo from middleware 1');
+    return res.json('Hello from middleware');
+})
 
 app.get('/api/users' , (req,res) => {
     return res.json(users);
@@ -12,12 +20,6 @@ app.get('/api/users' , (req,res) => {
 app.get('/users/' , (req,res) => {
     const html =  `<ul>${users.map(user => `<li>${user.first_name}</li>`).join("")}</ul>`;
     return  res.send(html);
-});
-
-app.get('/api/users/:id' , (req,res) => {
-    const id = Number(req.params.id);
-    const user = users.find(user => user.id === id);
-    return res.json(user);
 });
 
 
@@ -37,6 +39,28 @@ app.get('/users/:id' , (req,res) => {
     return res.send(html);
 
 });
+
+app.post("/api/users" , (req,res) => {
+    const body = req.body;
+    users.push({...body , id: users.length+1});
+    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users) , (err,data) => {
+        return res.json({status:'Success' , id: users.length});
+    });
+      
+});
+
+app.route('/api/users/:id')
+    .get((req,res) => {
+        const id = Number(req.params.id);
+        const user = users.find(user => user.id === id);
+        return res.json(user);
+    })
+    .patch((req,res) => {
+        return res.json({status:'Pendingg'});
+    })
+    .delete((req,res) => {
+        return res.json({status:'Pendingg'});
+    })
 
 
 app.listen(port , () => console.log("server started !!"));
